d78deb3e065dd033095f1f69b179d4fcd8a8bae7
gabriel <gmaher2@hotmail.com>
2017-07-10 19:52:05 -0700

    update train

--- python/tf3d.py
+++ python/tf3d.py
@@ -7,16 +7,28 @@ import matplotlib.pyplot as plt
 #######################################################
 # Get data
 #######################################################
-data_path = '/home/marsdenlab/datasets/DeepVesselData/'
-train = data_path+'train_dist72.hdf5'
-val = data_path+'val_dist72.hdf5'
-test = data_path+'test_dist72.hdf5'
-save_dir = './models/cnn'
+data_path = '/media/marsdenlab/Data1/datasets/DeepVessel/'
+train = data_path+'train_dist72_white_large_ct.hdf5'
+val = data_path+'val_dist72_white_large_ct.hdf5'
+test = data_path+'test_dist72_white_ct.hdf5'
+
+neg_train = data_path+'train_dist72_white_negative_ct.hdf5'
+neg_val = data_path+'val_dist72_white_negative_ct.hdf5'
 f_train = tables.open_file(train)
 f_val = tables.open_file(val)
 
+f_neg_train = tables.open_file(neg_train)
+f_neg_val = tables.open_file(neg_val)
+
 input_shape = f_train.root.X.shape
 output_shape = f_train.root.Y.shape
+print input_shape, output_shape
+
+print 'data shapes, train={},{}'.format(input_shape,output_shape)
+
+
+input_shape = f_neg_train.root.X.shape
+output_shape = f_neg_train.root.Y.shape
 print input_shape, output_shape
 
 print 'data shapes, train={},{}'.format(input_shape,output_shape)
@@ -25,27 +37,45 @@ print 'data shapes, train={},{}'.format(input_shape,output_shape)
 # Define variables
 ######################################################
 N = f_train.root.X.shape[0]
+N_neg = f_neg_train.root.X.shape[0]
 Nval = f_val.root.X.shape[0]
 W,H,D = f_train.root.X[0].shape
 C = 1
-Nbatch = 16
-lr = 5e-3
-Nsteps=5000
-print_step=100
-init = 1e-3
-Nlayers = 5
+Nbatch = 4
+lr = 1e-5
+Nsteps=15000
+print_step=1000
+init = 1.0e-3
+Nlayers = 8
+Nfilters = 32
+VESSEL_SCALE = 1000
+EPS=1e-4
+leaky_relu = tf.contrib.keras.layers.LeakyReLU(0.2)
+y_index=0
+alph = 0.3
+beta = 0.7
 #########################################################
 # Define graph
 #########################################################
-x = tf.placeholder(shape=[Nbatch,W,H,D,C],dtype=tf.float32)
-y = tf.placeholder(shape=[Nbatch,W,H,D,C],dtype=tf.float32)
+def selu(x):
+    with ops.name_scope('elu') as scope:
+        alpha = 1.6732632423543772848170429916717
+        scale = 1.0507009873554804934193349852946
+    return scale*tf.where(x>=0.0, x, alpha*tf.nn.elu(x)-alpha)
+#########################################################
+# Define graph
+#########################################################
+x = tf.placeholder(shape=[None,W,H,D,C],dtype=tf.float32)
+y = tf.placeholder(shape=[None,W,H,D,C],dtype=tf.float32)
 
-o_4 = tf_util.conv3D_N(x,N=Nlayers)
+yhat,yclass = tf_util.UNET3D(x,nfilters=Nfilters,activation=leaky_relu,init=init)
 
-yhat = tf_util.conv3D(o_4,tf.identity,nfilters=1,scope='yhat',init=init)
-yclass = tf.sigmoid(yhat)
-# yhat,yclass = tf_util.UNET3D(x)
-loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=y,logits=yhat,name='loss'))
+#loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=y,logits=yhat,name='loss'))
+
+TP = tf.reduce_sum(yclass*y)
+FP = tf.reduce_sum(yclass*(1-y))
+FN = tf.reduce_sum((1-yclass)*y)
+loss = -TP/(TP + alph*FP+beta*FN+EPS)
 
 opt = tf.train.AdamOptimizer(lr)
 train = opt.minimize(loss)
@@ -60,47 +90,23 @@ print yhat
 #######################################################
 train_hist = []
 val_hist = []
-sess = tf.Session()
-sess.run(tf.global_variables_initializer())
-saver = tf.train.Saver()
-for i in range(Nsteps):
-    xb,yb = tf_util.get_batch(f_train.root.X,f_train.root.Y,N,n=Nbatch,y_index=0)
+
+for i in range(Nsteps+1):
+    xb,yb = tf_util.get_batch(f_train.root.X,f_train.root.Y,N,n=3*Nbatch/4,y_index=y_index)
+    xbn,ybn = tf_util.get_batch(f_neg_train.root.X,f_neg_train.root.Y,N_neg,n=Nbatch/2,y_index=y_index)
+    xb = np.concatenate((xb,xbn),axis=0)
+    yb = np.concatenate((yb,ybn),axis=0)
     l,_=sess.run([loss,train],{x:xb,y:yb})
     if i%(print_step/5)==0:
         print "iteration {}".format(i)
-    if i%print_step == 0:
-        xb,yb = tf_util.get_batch(f_val.root.X,f_val.root.Y,Nval,Nbatch,y_index=0)
-        lval=sess.run(loss,{x:xb,y:yb})
-        print "Train: {}, Val: {}".format(l,lval)
+    if i%(print_step/5) == 0:
+        xb,yb = tf_util.get_batch(f_val.root.X,f_val.root.Y,Nval,Nbatch,y_index=1)
+        lval,ypred=sess.run([loss,yclass],{x:xb,y:yb})
+        print "Train: {}, Val: {}, p var {}, p mean {}, p max {}, p min {}".format(l,lval,
+                np.var(ypred),np.mean(ypred), np.amax(ypred),np.amin(ypred))
 
-saver.save(sess,'./models/cnn/cnn')
-######################################################
-# Plot
-######################################################
-from mpl_toolkits.axes_grid1 import make_axes_locatable
-plt.rcParams['figure.figsize'] = (20.0, 10.0)
-def implot(mp,ax):
-    im = ax.imshow(mp.astype(np.float32), cmap='gray')
-    divider = make_axes_locatable(ax)
-    cax = divider.append_axes("right", size="5%", pad=0.05)
-    cbar = plt.colorbar(im, cax=cax)
+        train_hist.append(l)
+        val_hist.append(lval)
 
-for i in range(5):
-    j = np.random.randint(len(f_val.root.X))
-    xval = f_val.root.X[j:j+2][:,:,:,:,np.newaxis]
-    yval = f_val.root.Y[j:j+2][:,:,:,:,0]
-    yval = yval[:,:,:,:,np.newaxis]
-    ypred = sess.run(yclass,{x:xval,y:yval})
-    mpx = np.amax(xval[0,:,:,:,0],axis=(1))
-    mpy = np.amax(yval[0,:,:,:,0],axis=(1))
-    mpd = np.amax(ypred[0,:,:,:,0],axis=(1))
-
-    plt.figure()
-    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, sharey=True)
-    implot(mpx,ax1)
-    implot(mpy,ax2)
-    implot(mpd,ax3)
-    plt.tight_layout
-    plt.savefig('{}.png'.format(i),dpi=400)
-
-plt.close('all')
+saver = tf.train.Saver()
+saver.save(sess,'./models/unet/unet')
