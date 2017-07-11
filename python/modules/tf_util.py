2409fcf9c357040a305b16a725b2721e9d7482e0
gabriel <gmaher2@hotmail.com>
2017-07-10 20:21:48 -0700

    fix

--- python/modules/tf_util.py
+++ python/modules/tf_util.py
@@ -23,18 +23,25 @@ def conv3D(x, activation=tf.nn.relu, shape=[3,3,3],nfilters=32, init=1e-3, scope
 
         h = tf.nn.convolution(x,W,padding='SAME',strides=[1,1,1], name='h')+b
 
-        a = activation(h, name='a')
+        a = activation(h)
 
         return a
 
+def repeat(x,axis,repeat):
+    s = x.get_shape().as_list()
+    splits = tf.split(value=x,num_or_size_splits=s[axis],axis=axis)
+    rep = [s for s in splits for _ in range(repeat)]
+    return tf.concat(rep,axis)
+
+def resize_tensor(x,scales=[1,2,2,2,1]):
+    out = x
+    for i in range(1,len(scales)):
+        out = repeat(out,i,scales[i])
+    return out
+
 def upsample3D(x, scope='upsample'):
     with tf.variable_scope(scope):
-        s = x.get_shape()
-        output_shape = [int(s[0]),2*int(s[1]),
-            2*int(s[2]),2*int(s[3]),int(s[4])]
-        W = tf.ones([2,2,2,s[4],s[4]],name='W')
-        o = tf.nn.conv3d_transpose(x,W,output_shape=output_shape,
-            strides=[1,1,1,1,1],padding='VALID', name='upsample')
+        o = resize_tensor(x)
         return o
 
 def conv3D_N(x, activation=tf.nn.relu, shape=[3,3,3],nfilters=32,init=1e-3,scope='conv3dn',N=2):
@@ -57,38 +64,38 @@ def resnet_conv3D_N(x, activation=tf.nn.relu, shape=[3,3,3],nfilters=32,init=1e-
 
     return y
 
-def unetBlock(x,nfilters=32,scope='unet3d'):
+def unetBlock(x,nfilters=32,scope='unet3d',init=1e-3,activation=tf.nn.relu):
     with tf.variable_scope(scope):
-        o1 = conv3D(x,nfilters=nfilters)
-        o2 = conv3D(o1,nfilters=2*nfilters)
+        o1 = conv3D(x,nfilters=nfilters,init=init,activation=activation)
+        o2 = conv3D(o1,nfilters=2*nfilters,init=init,activation=activation)
         o3 = tf.nn.pool(o2,[2,2,2],strides=[2,2,2],pooling_type='MAX',padding='SAME',name='pool')
     return o3,o2
 
-def unetUpsampleBlock(x,y,nfilters=32,scope='unet3dupsample'):
+def unetUpsampleBlock(x,y,activation=tf.nn.relu,init=1e-3,nfilters=32,scope='unet3dupsample'):
     with tf.variable_scope(scope):
         o = upsample3D(y)
         o = tf.concat([o,x],axis=4)
-        o = conv3D(o,nfilters=nfilters)
-        o = conv3D(o,nfilters=nfilters)
+        o = conv3D(o,nfilters=nfilters,init=init,activation=activation)
+        o = conv3D(o,nfilters=nfilters,init=init,activation=activation)
         return o
 
-def UNET3D(x,nfilters=32):
-    o1_down,o1 = unetBlock(x,nfilters,'layer1')
+def UNET3D(x,activation=tf.nn.relu,nfilters=32,init=1e-3):
+    o1_down,o1 = unetBlock(x,nfilters=nfilters,scope='layer1',init=init,activation=activation)
     print o1,o1_down
-    o2_down,o2 = unetBlock(o1_down,2*nfilters,'layer2')
+    o2_down,o2 = unetBlock(o1_down,nfilters=2*nfilters,scope='layer2',init=init,activation=activation)
     print o2,o2_down
-    o3_down,o3 = unetBlock(o2_down,4*nfilters,'layer3')
+    o3_down,o3 = unetBlock(o2_down,nfilters=4*nfilters,scope='layer3',init=init,activation=activation)
     print o3,o3_down
-    # o4_down,o4 = unetBlock(o3_down,8*nfilters,'layer4')
-    # print o4,o4_down
+    o4_down,o4 = unetBlock(o3_down,nfilters=8*nfilters,scope='layer4',init=init,activation=activation)
+    print o4,o4_down
 
-    # a_3 = unetUpsampleBlock(o3,o4,8*nfilters,'o_layer3')
-    # print a_3
-    a_2 = unetUpsampleBlock(o2,o3,4*nfilters,'o_layer2')
+    a_3 = unetUpsampleBlock(o3,o4,nfilters=8*nfilters,scope='o_layer3',init=init,activation=activation)
+    print a_3
+    a_2 = unetUpsampleBlock(o2,a_3,nfilters=4*nfilters,scope='o_layer2',init=init,activation=activation)
     print a_2
-    a_1 = unetUpsampleBlock(o1,a_2,2*nfilters,'o_layer1')
+    a_1 = unetUpsampleBlock(o1,a_2,nfilters=2*nfilters,scope='o_layer1',init=init,activation=activation)
     print a_1
-    yhat = conv3D(a_1,tf.identity,shape=[1,1,1],nfilters=1,scope='yhat')
+    yhat = conv3D(a_1,tf.identity,shape=[1,1,1],nfilters=1,scope='yhat',init=init)
     print yhat
     yclass = tf.sigmoid(yhat)
     print yclass
--- python/segment_volume.py
+++ python/segment_volume.py
@@ -15,10 +15,29 @@ vol = args.volume
 
 img = sitk.ReadImage(vol)
 img_np = sitk.GetArrayFromImage(img)
-img_np = (1.0*img_np-np.amin(img_np))/(np.amax(img_np)-np.amin(img_np))
-save_dir = './models/cnn/cnn'
+#img_np = (1.0*img_np-np.amin(img_np))/(np.amax(img_np)-np.amin(img_np))
+img_np = (1.0*img_np-np.mean(img_np))/(np.std(img_np))
+
+save_dir = './models/unet/unet'
 
 print 'image shape {}'.format(img_np.shape)
+
+#######################################################
+# Get data
+#######################################################
+# data_path = '/home/marsdenlab/datasets/DeepVesselData/'
+# train = data_path+'train_dist72.hdf5'
+# val = data_path+'val_dist72.hdf5'
+# test = data_path+'test_dist72.hdf5'
+# save_dir = './models/unet/unet'
+# f_train = tables.open_file(train)
+# f_val = tables.open_file(val)
+#
+# input_shape = f_train.root.X.shape
+# output_shape = f_train.root.Y.shape
+# print input_shape, output_shape
+#
+# print 'data shapes, train={},{}'.format(input_shape,output_shape)
 
 ######################################################
 # Define variables
@@ -28,24 +47,58 @@ H = 72
 D = 72
 C = 1
 init = 1e-3
-Nlayers = 6
+Nlayers = 8
 stride = 16
+Nfilters=10
 Nx,Ny,Nz = img_np.shape
+leaky_relu = tf.contrib.keras.layers.LeakyReLU(0.2)
 #########################################################
 # Define graph
 #########################################################
 x = tf.placeholder(shape=[None,W,H,D,C],dtype=tf.float32)
 y = tf.placeholder(shape=[None,W,H,D,C],dtype=tf.float32)
 
-o_4 = tf_util.conv3D_N(x,N=Nlayers)
-
-yhat = tf_util.conv3D(o_4,tf.identity,nfilters=1,scope='yhat',init=init)
-yclass = tf.sigmoid(yhat)
+# o_4 = tf_util.conv3D_N(x,N=Nlayers,nfilters=Nfilters)
+#
+# yhat = tf_util.conv3D(o_4,tf.identity,nfilters=1,scope='yhat',init=init)
+# yclass = tf.sigmoid(yhat)
+yhat,yclass = tf_util.UNET3D(x,nfilters=Nfilters,activation=leaky_relu,init=init)
 
 sess = tf.Session()
 
 saver = tf.train.Saver()
 saver.restore(sess,save_dir)
+
+######################################################
+# Plot
+######################################################
+# from mpl_toolkits.axes_grid1 import make_axes_locatable
+# plt.rcParams['figure.figsize'] = (20.0, 10.0)
+# def implot(mp,ax):
+#     im = ax.imshow(mp.astype(np.float32), cmap='gray')
+#     divider = make_axes_locatable(ax)
+#     cax = divider.append_axes("right", size="5%", pad=0.05)
+#     cbar = plt.colorbar(im, cax=cax)
+#
+# for i in range(5):
+#     j = np.random.randint(len(f_val.root.X))
+#     xval = f_val.root.X[j:j+2][:,:,:,:,np.newaxis]
+#     yval = f_val.root.Y[j:j+2][:,:,:,:,0]
+#     yval = yval[:,:,:,:,np.newaxis]
+#     ypred = sess.run(yclass,{x:xval,y:yval})
+#     mpx = np.amax(xval[0,:,:,:,0],axis=(1))
+#     mpy = np.amax(yval[0,:,:,:,0],axis=(1))
+#     mpd = np.amax(ypred[0,:,:,:,0],axis=(1))
+#
+#     plt.figure()
+#     fig, (ax1, ax2, ax3) = plt.subplots(1, 3, sharey=True)
+#     implot(mpx,ax1)
+#     implot(mpy,ax2)
+#     implot(mpd,ax3)
+#     plt.tight_layout
+#     plt.savefig('{}.png'.format(i),dpi=400)
+#
+# plt.close('all')
 
 ##########################################################
 # Segment volume
